068d29b6c5c6d4d7b2c170f6b832cc087734ce16
MehdiRouis <mehdi.rouis.1@laposte.net>
2018-09-21 14:58:07 +0200

    First commit

---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(jQuery).ready(function() {
+    $('#home').click(function() {
+      document.getElementById('home').className = 'nav-link active';
+      document.getElementById('skills').className = 'nav-link';
+      document.getElementById('contact').className = 'nav-link';
+      $('#content-skills').slideUp();
+      $('#content-contact').slideUp();
+      $('#content-home').slideDown();
+    });
+    $('#skills').click(function() {
+      document.getElementById('skills').className = 'nav-link active';
+      document.getElementById('home').className = 'nav-link';
+      document.getElementById('contact').className = 'nav-link';
+      $('#content-home').slideUp();
+      $('#content-contact').slideUp();
+      $('#content-skills').slideDown();
+    });
+    $('#contact').click(function() {
+      document.getElementById('contact').className = 'nav-link active';
+      document.getElementById('home').className = 'nav-link';
+      document.getElementById('skills').className = 'nav-link';
+      $('#content-home').slideUp();
+      $('#content-skills').slideUp();
+      $('#content-contact').slideDown();
+    });
+  })
+});
